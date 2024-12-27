8ee07e14b02dab8e40c45187325f2622f8d7b8a2
devesh1808 <devesh1808@gmail.com>
2024-12-28 03:33:02 +0530

    added icons and added some styling to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,41 @@ import "../App.css";
 function Header(props) {
   return (
     <div className="flex shopping-card">
-      <div onClick={() => props.handleShow(false)}>Shopping Cart App</div>
-      <div onClick={() => props.handleShow(true)}>
-        {" "}
-        Cart
-        <sup> {props.count} </sup>
+      <div>
+        <span>
+          <img
+            src="https://icon2.cleanpng.com/lnd/20241010/aa/ebe4f51c75ef9cb5aea4a7d85f9f79.webp"
+            alt=""
+            height={20}
+            style={{ marginBottom: "-4px" }}
+          />
+        </span>
+        <span
+          className="text-style"
+          style={{ marginLeft: "8px" }}
+          onClick={() => props.handleShow(false)}
+        >
+          My Cart
+        </span>
+      </div>
+      <div>
+        <span>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
+            alt="shopping cart icon"
+            height={23}
+            style={{ marginBottom: "-3px" }}
+          />
+        </span>
+        <span
+          className="text-style"
+          style={{ marginLeft: "6px" }}
+          onClick={() => props.handleShow(true)}
+        >
+          {" "}
+          Cart
+          <sup> {props.count} </sup>
+        </span>
       </div>
     </div>
   );
